80c3683676bc1ffe302f25c6cb46f853224c5b2e
amananurag <amananurag.20@gmail.com>
2025-07-08 10:31:34 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import Mobile from "./components/Mobile.jsx"
 
 const App = () => {
+
+  const [storeData, setStoreData]=useState([]);
+  const [count, setCount]=useState(0);
+
+  const fetchData=async()=>{
+    
+    const response= await fetch("https://fakestoreapi.com/products");
+
+    const data= await response.json();
+  
+    setStoreData(data)
+
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+console.log(storeData)
   return (
     <div>
       <Mobile name={"vivo-t4-5g"} price={14000}
