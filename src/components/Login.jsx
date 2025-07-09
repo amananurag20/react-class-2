3a65ca15c86d4ddc329da2a1e1f906484bf728ab
amananurag <amananurag.20@gmail.com>
2025-07-09 11:25:35 +0530

    value

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from "react";
+import "./Login.css"
 
 const Login = () => {
+
+const [email, setEmail]=useState("");
+const [password, setPassword]= useState("")
+
+
+    const handleChange=(event)=>{
+        setEmail(event.target.value);
+    }
+
+    const handlePassword=(e)=>{
+        setPassword(e.target.value);
+    }
+
+    console.log(email)
+    console.log(password)
   return (
-    <div>
-      <img src='https://static.vecteezy.com/system/resources/previews/041/731/090/non_2x/login-icon-vector.jpg'></img>
+    <div className="input-div">
+      
+      <label htmlFor='email'>Email</label>
+      <input placeholder='Email' id='email' onChange={handleChange} value={email}></input>
+      <label htmlFor='pass'>Password</label>
+      <input type='password' placeholder='password' id='pass' value={password} onChange={handlePassword}></input>
+
+      <button>Login</button>
+      
     </div>
   )
 }
